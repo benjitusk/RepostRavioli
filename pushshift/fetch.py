a1beb98b09f357538b598ae0a5793e7204030c39
Benji Tusk <benjitusk1@gmail.com>
2021-04-07 15:14:24 +0300

    mistyped time as ctime

--- pushshift/fetch.py
+++ pushshift/fetch.py
@@ -63,35 +63,35 @@ def main():
         print(f'Starting archival for {month} {start_year}...')
         print(
             f'Starting submissions fetch for {month} {start_year} [{start_timestamp} - {end_timestamp}] at {time.ctime()}...')
-        start_timer = time.ctime()
+        start_timer = time.time()
         new_fetch = list(api.search_submissions(
             after=start_timestamp, before=end_timestamp, subreddit=sub))
         print(
-            f'Finished submissions fetch for {month} {start_year} after {get_time_from_seconds(time.ctime()-start_timer)}. This month contains {len(new_fetch)} submissions.')
+            f'Finished submissions fetch for {month} {start_year} after {get_time_from_seconds(time.time()-start_timer)}. This month contains {len(new_fetch)} submissions.')
         total_entries += len(new_fetch)
         month_entries += len(new_fetch)
         print('Writing new submissions to disk')
-        start_timer = time.ctime()
+        start_timer = time.time()
         with open(f'{month}.{start_year}.submissions.json', 'w') as output:
             json.dump(new_fetch, output)
         print(
-            f'Done. Writing submissions to disk completed after: {get_time_from_seconds(time.ctime()-start_timer)}')
+            f'Done. Writing submissions to disk completed after: {get_time_from_seconds(time.time()-start_timer)}')
         new_fetch = None
         print(
             f'Starting comments fetch for {month} {start_year} [{start_timestamp} - {end_timestamp}] at {time.ctime()}...')
-        start_timer = time.ctime()
+        start_timer = time.time()
         new_fetch = list(api.search_comments(
             after=start_timestamp, before=end_timestamp, subreddit=sub))
         print(
-            f'Finished comments fetch for {month} {start_year} after {get_time_from_seconds(time.ctime()-start_timer)}. This month contains {len(new_fetch)} comments.')
+            f'Finished comments fetch for {month} {start_year} after {get_time_from_seconds(time.time()-start_timer)}. This month contains {len(new_fetch)} comments.')
         total_entries += len(new_fetch)
         month_entries += len(new_fetch)
         print('Writing new comments to disk')
-        start_timer = time.ctime()
+        start_timer = time.time()
         with open(f'{month}.{start_year}.comments.json', 'w') as output:
             json.dump(new_fetch, output)
         print(
-            f'Done. Writing comments to disk completed after: {get_time_from_seconds(time.ctime()-start_timer)}')
+            f'Done. Writing comments to disk completed after: {get_time_from_seconds(time.time()-start_timer)}')
         new_fetch = None
         print(
             f'Finished archiving {month_entries} entries for {month} {start_year}.')
@@ -105,7 +105,7 @@ def main():
 
 
 def get_time_from_seconds(sec):
-    return str(datetime.timedelta(seconds=(time.ctime() - sec)))
+    return str(datetime.timedelta(seconds=(time.time() - sec)))
 
 
 def name_of_month(month_number):
